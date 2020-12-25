1acee2f514b6bea924902bd893a7073969d637ed
Snaggly <oemshani@yahoo.com>
2020-12-25 14:21:16 +0100

    Added rest of the KeyEvent Icons

--- app/src/main/java/com/snaggly/ksw_toolkit/util/applist/KeyEventMock.kt
+++ app/src/main/java/com/snaggly/ksw_toolkit/util/applist/KeyEventMock.kt
@@ -23,6 +23,17 @@ class KeyEventMock {
             result.add(AppInfo(KeyCode.ENVELOPE.name, "", ContextCompat.getDrawable(context, R.drawable.ic_baseline_email_36)!!))
             result.add(AppInfo(KeyCode.EXPLORER.name, "", ContextCompat.getDrawable(context, R.drawable.ic_baseline_file_copy_36)!!))
             result.add(AppInfo(KeyCode.HOME.name, "", ContextCompat.getDrawable(context, R.drawable.ic_baseline_home_24)!!))
+            result.add(AppInfo(KeyCode.MEDIA_NEXT.name, "", ContextCompat.getDrawable(context, R.drawable.ic_baseline_skip_next_36)!!))
+            result.add(AppInfo(KeyCode.MEDIA_PLAY_PAUSE.name, "", ContextCompat.getDrawable(context, R.drawable.ic_baseline_play_arrow_36)!!))
+            result.add(AppInfo(KeyCode.MEDIA_PREVIOUS.name, "", ContextCompat.getDrawable(context, R.drawable.ic_baseline_skip_previous_36)!!))
+            result.add(AppInfo(KeyCode.MEDIA_STOP.name, "", ContextCompat.getDrawable(context, R.drawable.ic_baseline_stop_36)!!))
+            result.add(AppInfo(KeyCode.MUSIC.name, "", ContextCompat.getDrawable(context, R.drawable.ic_baseline_library_music_36)!!))
+            result.add(AppInfo(KeyCode.MUTE.name, "", ContextCompat.getDrawable(context, R.drawable.ic_baseline_volume_mute_36)!!))
+            result.add(AppInfo(KeyCode.SETTINGS.name, "", ContextCompat.getDrawable(context, R.drawable.ic_baseline_settings_24)!!))
+            result.add(AppInfo(KeyCode.VOICE_ASSIST.name, "", ContextCompat.getDrawable(context, R.drawable.ic_baseline_record_voice_over_36)!!))
+            result.add(AppInfo(KeyCode.VOLUME_DOWN.name, "", ContextCompat.getDrawable(context, R.drawable.ic_baseline_volume_down_36)!!))
+            result.add(AppInfo(KeyCode.VOLUME_MUTE.name, "", ContextCompat.getDrawable(context, R.drawable.ic_baseline_volume_off_36)!!))
+            result.add(AppInfo(KeyCode.VOLUME_UP.name, "", ContextCompat.getDrawable(context, R.drawable.ic_baseline_volume_up_36)!!))
 
             return result
         }
