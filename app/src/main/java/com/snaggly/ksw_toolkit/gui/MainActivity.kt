1fc5478d867ac0a069bfc90f94e0ff55445be851
Snaggly <oemshani@yahoo.com>
2021-01-06 17:50:10 +0100

    Preventing MainActivity from starting Service before getting permission

--- app/src/main/java/com/snaggly/ksw_toolkit/gui/MainActivity.kt
+++ app/src/main/java/com/snaggly/ksw_toolkit/gui/MainActivity.kt
@@ -1,19 +1,15 @@
 package com.snaggly.ksw_toolkit.gui
 
 import android.content.Intent
-import android.content.pm.PackageManager
 import android.net.Uri
-import android.os.Build
 import android.os.Bundle
 import android.provider.Settings
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import com.snaggly.ksw_toolkit.R
 import com.snaggly.ksw_toolkit.core.service.McuService
-import java.util.jar.Manifest
 
 class MainActivity : AppCompatActivity() {
     private lateinit var mcuServiceIntent: Intent
@@ -34,15 +30,19 @@ class MainActivity : AppCompatActivity() {
     private var tabFragmentId = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        if (checkPermissions()) {
-            startApp()
-        }
+        if (!checkPermissions())
+            finish()
     }
 
     override fun onStart() {
         super.onStart()
         mcuServiceIntent = Intent(this, McuService::class.java)
         startService(mcuServiceIntent)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        startApp()
     }
 
     override fun onStop() {
