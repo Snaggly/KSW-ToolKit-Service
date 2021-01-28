9c04fd63f45f5683d9204e343be2663bc1e6be80
Snaggly <oemshani@yahoo.com>
2021-01-28 12:32:46 +0100

    Removed useless KeyEvents. Adjusting for Voice Assist

--- app/src/main/java/com/snaggly/ksw_toolkit/util/keyevent/KeyEvent.kt
+++ app/src/main/java/com/snaggly/ksw_toolkit/util/keyevent/KeyEvent.kt
@@ -11,7 +11,6 @@ class KeyEvent(name: String, icon: Drawable, val code: Int) : ListType(name, ico
     companion object {
         fun getKeyEventList(context: Context) : ArrayList<KeyEvent> {
             val result = ArrayList<KeyEvent>()
-            result.add(KeyEvent(KeyCode.ASSIST.name, ContextCompat.getDrawable(context, R.drawable.ic_baseline_record_voice_over_36)!!, KeyCode.ASSIST.keycode))
             result.add(KeyEvent(KeyCode.BACK.name, ContextCompat.getDrawable(context, R.drawable.ic_baseline_keyboard_backspace_36)!!, KeyCode.BACK.keycode))
             result.add(KeyEvent(KeyCode.CALCULATOR.name, ContextCompat.getDrawable(context, R.drawable.ic_baseline_calculate_36)!!, KeyCode.CALCULATOR.keycode))
             result.add(KeyEvent(KeyCode.CALENDAR.name, ContextCompat.getDrawable(context, R.drawable.ic_baseline_calendar_today_36)!!, KeyCode.CALENDAR.keycode))
@@ -21,7 +20,6 @@ class KeyEvent(name: String, icon: Drawable, val code: Int) : ListType(name, ico
             result.add(KeyEvent(KeyCode.DPAD_LEFT.name, ContextCompat.getDrawable(context, R.drawable.ic_baseline_keyboard_arrow_left_36)!!, KeyCode.DPAD_LEFT.keycode))
             result.add(KeyEvent(KeyCode.DPAD_RIGHT.name, ContextCompat.getDrawable(context, R.drawable.ic_baseline_keyboard_arrow_right_36)!!, KeyCode.DPAD_RIGHT.keycode))
             result.add(KeyEvent(KeyCode.DPAD_UP.name, ContextCompat.getDrawable(context, R.drawable.ic_baseline_keyboard_arrow_up_36)!!, KeyCode.DPAD_UP.keycode))
-            result.add(KeyEvent(KeyCode.ENTER.name, ContextCompat.getDrawable(context, R.drawable.ic_baseline_filter_center_focus_24)!!, KeyCode.ENTER.keycode))
             result.add(KeyEvent(KeyCode.ENVELOPE.name, ContextCompat.getDrawable(context, R.drawable.ic_baseline_email_36)!!, KeyCode.ENVELOPE.keycode))
             result.add(KeyEvent(KeyCode.EXPLORER.name, ContextCompat.getDrawable(context, R.drawable.ic_baseline_file_copy_36)!!, KeyCode.EXPLORER.keycode))
             result.add(KeyEvent(KeyCode.HOME.name,  ContextCompat.getDrawable(context, R.drawable.ic_baseline_home_24)!!, KeyCode.HOME.keycode))
@@ -30,7 +28,6 @@ class KeyEvent(name: String, icon: Drawable, val code: Int) : ListType(name, ico
             result.add(KeyEvent(KeyCode.MEDIA_PREVIOUS.name, ContextCompat.getDrawable(context, R.drawable.ic_baseline_skip_previous_36)!!, KeyCode.MEDIA_PREVIOUS.keycode))
             result.add(KeyEvent(KeyCode.MEDIA_STOP.name, ContextCompat.getDrawable(context, R.drawable.ic_baseline_stop_36)!!, KeyCode.MEDIA_STOP.keycode))
             result.add(KeyEvent(KeyCode.MUSIC.name, ContextCompat.getDrawable(context, R.drawable.ic_baseline_library_music_36)!!, KeyCode.MUSIC.keycode))
-            result.add(KeyEvent(KeyCode.MUTE.name, ContextCompat.getDrawable(context, R.drawable.ic_baseline_volume_mute_36)!!, KeyCode.MUTE.keycode))
             result.add(KeyEvent(KeyCode.SETTINGS.name, ContextCompat.getDrawable(context, R.drawable.ic_baseline_settings_24)!!, KeyCode.SETTINGS.keycode))
             result.add(KeyEvent(KeyCode.VOICE_ASSIST.name, ContextCompat.getDrawable(context, R.drawable.ic_baseline_record_voice_over_36)!!, KeyCode.VOICE_ASSIST.keycode))
             result.add(KeyEvent(KeyCode.VOLUME_DOWN.name, ContextCompat.getDrawable(context, R.drawable.ic_baseline_volume_down_36)!!, KeyCode.VOLUME_DOWN.keycode))
