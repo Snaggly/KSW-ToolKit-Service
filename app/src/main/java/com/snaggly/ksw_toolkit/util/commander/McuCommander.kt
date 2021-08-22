61ee51b4d6977ea4d46b91880eb0636435e7be60
Snaggly <oemshani@yahoo.com>
2021-08-22 10:35:03 +0200

    McuCommand - BrightnessUp/Down fix
    Redundant to send directly to Mcu, as there is a SysObserver to send anyways.

--- app/src/main/java/com/snaggly/ksw_toolkit/util/commander/McuCommander.kt
+++ app/src/main/java/com/snaggly/ksw_toolkit/util/commander/McuCommander.kt
@@ -15,21 +15,18 @@ object McuCommander {
             }
 
             McuCommandsEnum.BrightnessInc -> {
-                var currentBrightness = (Settings.System.getInt(context.contentResolver, Settings.System.SCREEN_BRIGHTNESS, 255) * 100) / 255
-                currentBrightness += 10
-                if (currentBrightness > 100)
-                    currentBrightness = 100
-                Settings.System.putInt(context.contentResolver, Settings.System.SCREEN_BRIGHTNESS, currentBrightness * 255 / 100)
-                mcuCommunicator?.sendCommand(McuCommands.SetBrightnessLevel(currentBrightness.toByte()))
+                var currentBrightness = Settings.System.getInt(context.contentResolver, Settings.System.SCREEN_BRIGHTNESS, 0xFF)
+                currentBrightness += 51
+                currentBrightness = currentBrightness.and(0xFF)
+                Settings.System.putInt(context.contentResolver, Settings.System.SCREEN_BRIGHTNESS, currentBrightness)
             }
 
             McuCommandsEnum.BrightnessDec -> {
-                var currentBrightness = (Settings.System.getInt(context.contentResolver, Settings.System.SCREEN_BRIGHTNESS, 255) * 100) / 255
-                currentBrightness -= 10
+                var currentBrightness = Settings.System.getInt(context.contentResolver, Settings.System.SCREEN_BRIGHTNESS, 0xFF)
+                currentBrightness -= 51
                 if (currentBrightness < 0)
                     currentBrightness = 0
-                Settings.System.putInt(context.contentResolver, Settings.System.SCREEN_BRIGHTNESS, currentBrightness * 255 / 100)
-                mcuCommunicator?.sendCommand(McuCommands.SetBrightnessLevel(currentBrightness.toByte()))
+                Settings.System.putInt(context.contentResolver, Settings.System.SCREEN_BRIGHTNESS, currentBrightness)
             }
 
             McuCommandsEnum.CarInfo -> mcuCommunicator?.sendCommand(McuCommands.SWITCH_TO_OEM)
