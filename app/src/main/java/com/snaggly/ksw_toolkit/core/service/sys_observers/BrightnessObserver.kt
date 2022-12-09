bf0a0f9460f8497e2c981c3e1f5106126a5e229a
cocoakaike <akaike@cocoa.ne.jp>
2022-12-10 00:55:19 +0900

    Fixes when Night Brightness function is restored

--- app/src/main/java/com/snaggly/ksw_toolkit/core/service/sys_observers/BrightnessObserver.kt
+++ app/src/main/java/com/snaggly/ksw_toolkit/core/service/sys_observers/BrightnessObserver.kt
@@ -4,15 +4,16 @@ import android.content.Context
 import android.database.ContentObserver
 import android.os.Handler
 import android.provider.Settings
+import android.util.Log
 import com.snaggly.ksw_toolkit.core.service.mcu.McuLogic
 import projekt.auto.mcu.ksw.serial.collection.McuCommands
 
 class BrightnessObserver(private val context: Context) {
-
     private val brightnessObserver = object : ContentObserver(Handler(context.mainLooper)) {
         override fun onChange(selfChange: Boolean) {
             val newBrightness = Settings.System.getInt(context.contentResolver, Settings.System.SCREEN_BRIGHTNESS)
             McuLogic.mcuCommunicator?.sendCommand(McuCommands.SetBrightnessLevel((newBrightness * 100 / 255).toByte()))
+            Log.d("KSWToolKit", "D 3:" + (newBrightness * 100 / 255).toString() + " / " + Settings.System.SCREEN_BRIGHTNESS)
         }
     }
 
