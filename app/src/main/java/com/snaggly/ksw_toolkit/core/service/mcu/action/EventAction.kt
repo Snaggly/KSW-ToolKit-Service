dde96ffb0aa2d9aea1ccc6e1be310c712e834d83
akaike <akaike@cocoa.ne.jp>
2022-12-19 14:49:26 +0900

    When using AutoKit, change the dial operation to DPAD left/right; might be better to be able to turn it on/off

--- app/src/main/java/com/snaggly/ksw_toolkit/core/service/mcu/action/EventAction.kt
+++ app/src/main/java/com/snaggly/ksw_toolkit/core/service/mcu/action/EventAction.kt
@@ -21,13 +21,17 @@ open class EventAction(private val context: Context) {
             val eventConfig = config.eventManagers[event]
             when (eventConfig?.eventMode) {
                 EventMode.KeyEvent -> {
+                    if (AutoKitCallBackImpl.isUsing()) when (event) {
+                        EventManagerTypes.KnobTurnLeft -> eventConfig.keyCode = KeyCode.DPAD_LEFT.keycode
+                        EventManagerTypes.KnobTurnRight -> eventConfig.keyCode = KeyCode.DPAD_RIGHT.keycode
+                    }
                     if (!(McuLogic.actionLock && eventConfig.keyCode == KeyCode.HOME.keycode))
                         KeyInjector.sendKey(eventConfig.keyCode)
                     when (eventConfig.keyCode) {
                         KeyCode.DPAD_UP.keycode -> AutoKitCallBackImpl.drapUp(context.applicationContext)
                         KeyCode.DPAD_DOWN.keycode -> AutoKitCallBackImpl.drapDown(context.applicationContext)
                         KeyCode.ENTER.keycode -> AutoKitCallBackImpl.enter(context.applicationContext)
-                        KeyCode.DPAD_RIGHT.keycode ->  {
+                        KeyCode.DPAD_RIGHT.keycode -> {
                             AutoKitCallBackImpl.drapRight(context.applicationContext)
                             AutoNavi.dragRight(context.applicationContext)
                         }
@@ -36,11 +40,11 @@ open class EventAction(private val context: Context) {
                             AutoNavi.dragLeft(context.applicationContext)
                         }
                         KeyCode.CALL.keycode -> {
-                            WitsCommand.sendCommand(7, 113, "");
+                            WitsCommand.sendCommand(7, 113, "")
                             CallHandler.handleAccept(context.applicationContext)
                         }
                         KeyCode.ENDCALL.keycode -> {
-                            WitsCommand.sendCommand(7, 113, "");
+                            WitsCommand.sendCommand(7, 113, "")
                             CallHandler.handleReject(context.applicationContext)
                         }
                         KeyCode.APP_SWITCH.keycode -> WitsCommand.sendCommand(7, 101, "")
